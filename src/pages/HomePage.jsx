5e8c6b5eeed007589825c8503a8c3303c48bbed3
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-18 15:31:16 +0100

    homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,17 @@ export default function Homepage() {
   return (
     <>
       <Header />
-      <h1>Ciao!</h1>
+      <main className="container-sm mt-4">
+        <h1 className="text-center">Benvenuti!</h1>
+        <h3 className="text-center">
+          Qui la moda prende vita: nuovi arrivi, tendenze del momento e outfit
+          perfetti per ogni occasione. Siamo felici di aiutarvi a creare il
+          vostro stile unico!
+        </h3>
+        <div className="d-flex justify-content-center">
+          <img src="/promotion-fashion-banner_1188-223.avif" alt="jumbotron" />
+        </div>
+      </main>
     </>
   );
 }
