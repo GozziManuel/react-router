525c08ea13f3cb860624e7407c6e203600b976ad
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-18 16:34:57 +0100

    productCard

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function ProductCard() {
+  const [products, setProducts] = useState([]);
+  const apiGetProducts = () => {
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      setProducts(res.data);
+      console.log(res.data);
+    });
+  };
+  useEffect(apiGetProducts, []);
+  return (
+    <>
+      {products.map((element, index) => (
+        <div className="col-6" key={index}>
+          <div className="card h-100 justify-content-center">
+            <img
+              src={element.image}
+              className="card-img-top cardimg"
+              alt="Product"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{element.title}</h5>
+              <p className="card-text">{element.description}</p>
+              <p className="fw-bold">{element.price}&euro;</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,37 +1,13 @@
-import { useEffect, useState } from "react";
 import Header from "../components/Header";
-import axios from "axios";
+import ProductCard from "../components/ProductCard";
 
 export default function ProductPage() {
-  const [products, setProducts] = useState([]);
-  const apiGetProducts = () => {
-    axios.get("https://fakestoreapi.com/products").then((res) => {
-      setProducts(res.data);
-      console.log(res.data);
-    });
-  };
-  useEffect(apiGetProducts, []);
   return (
     <>
       <Header />
       <main className="container-sm my-5">
         <div className="row g-3">
-          {products.map((element, index) => (
-            <div className="col-6">
-              <div className="card h-100 justify-content-center" key={index}>
-                <img
-                  src={element.image}
-                  className="card-img-top cardimg"
-                  alt="Product"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{element.title}</h5>
-                  <p className="card-text">{element.description}</p>
-                  <p className="fw-bold">{element.price}&euro;</p>
-                </div>
-              </div>
-            </div>
-          ))}
+          <ProductCard />
         </div>
       </main>
     </>
