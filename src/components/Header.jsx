6d9cc9058cc7afeab9f880170ab5f98017656e46
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-18 15:40:12 +0100

    Working Buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <nav
@@ -5,20 +7,20 @@ export default function Header() {
       data-bs-theme="dark"
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <NavLink className="navbar-brand" to="/">
           Home
-        </a>
+        </NavLink>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link " aria-current="page" href="#">
+              <NavLink className="nav-link" to="/ProductPage">
                 Products
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <NavLink className="nav-link" to="/AboutUs">
                 About US
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
