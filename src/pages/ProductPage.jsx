f05b1fc813342e9b17aa80812d5760fa9fe9017d
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-18 15:51:58 +0100

    usestate and useeffect

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,10 +1,15 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
 
 export default function ProductPage() {
+  const [products, setProducts] = useState([]);
+  const apiGetProducts = () => {
+    axios.get("https://fakestoreapi.com/").then((res) => setProducts(res.data));
+  };
   return (
     <>
       <Header />
-      <h1>Prodotti!</h1>
     </>
   );
 }
