7f678fc6dccc50ecd207badf72e4b0be04f24ac6
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-19 17:17:14 +0100

    err

--- src/pages/DetailedProductPage.jsx
+++ src/pages/DetailedProductPage.jsx
@@ -3,18 +3,17 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function DetailedProductPage() {
-  const [detailedProduct, SetdetailedProduct] = useState([]);
   const { id } = useParams();
+  const [detailedProduct, SetdetailedProduct] = useState([]);
+
   const GetIdProduct = () => {
-    axios
-      .get(`https://fakestoreapi.com/products/${id}`)
-      .then((res) => {
-        console.log(res.data);
-        SetdetailedProduct(res.data);
-      })
-      .catch((error) => {
-        if (error?.response?.status === 404) useNavigate("/404");
-      });
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
+      console.log(res.data);
+      SetdetailedProduct(res.data);
+    });
+    //   .catch((error) => {
+    //     if (error?.response?.status === 404) useNavigate("/404");
+    //   });
   };
   useEffect(GetIdProduct, []);
   if (!detailedProduct) return <h1>Loading...</h1>;
