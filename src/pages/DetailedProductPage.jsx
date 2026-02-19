4601ed5086eb95ecbbdbe080284c65dc9efc5e1c
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-20 00:40:46 +0100

    error throw and catch + 404 page

--- src/pages/DetailedProductPage.jsx
+++ src/pages/DetailedProductPage.jsx
@@ -5,15 +5,30 @@ import { useNavigate, useParams } from "react-router-dom";
 export default function DetailedProductPage() {
   const { id } = useParams();
   const [detailedProduct, SetdetailedProduct] = useState([]);
+  const navigate = useNavigate();
+  const navigateButton = () => {};
 
   const GetIdProduct = () => {
-    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
-      console.log(res.data);
-      SetdetailedProduct(res.data);
-    });
-    //   .catch((error) => {
-    //     if (error?.response?.status === 404) useNavigate("/404");
-    //   });
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => {
+        console.log(res.data);
+
+        if (!res.data) {
+          throw new Error("ProductNotFound");
+        }
+
+        SetdetailedProduct(res.data);
+      })
+      .catch((error) => {
+        if (
+          error?.response?.status === 404 ||
+          error.message === "ProductNotFound"
+        ) {
+          navigate("/404");
+          console.info("An unexpected error occurred:", error);
+        }
+      });
   };
   useEffect(GetIdProduct, []);
   if (!detailedProduct) return <h1>Loading...</h1>;
@@ -22,6 +37,10 @@ export default function DetailedProductPage() {
       <h1>{detailedProduct.title}</h1>
       <img src={detailedProduct.image} alt={detailedProduct.title} />
       <p>{detailedProduct.description}</p>
+      <button className="btn btn-primary" onClick={navigateButton}>
+        Indietro
+      </button>
+      <button className="btn btn-primary">Avanti</button>
     </>
   );
 }
