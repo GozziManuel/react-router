47b4b8d40af58a065935b24a587b116866993baf
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-19 15:12:23 +0100

    param

--- src/pages/DetailedProductPage.jsx
+++ src/pages/DetailedProductPage.jsx
@@ -1,5 +1,13 @@
 import { useParams } from "react-router-dom";
 
 export default function DetailedProductPage() {
-  return;
+  const { id } = useParams();
+  const GetIdProduct = (id) => {
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
+      console.log(res.data);
+    });
+  };
+  useEffect(() => GetIdProduct, []);
+
+  return <h1>prodotto {id}</h1>;
 }
