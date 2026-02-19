30943aafd3a316a7eadb75a774156b9f1e32b6be
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-19 15:51:54 +0100

    created full card

--- src/pages/DetailedProductPage.jsx
+++ src/pages/DetailedProductPage.jsx
@@ -1,15 +1,23 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function DetailedProductPage() {
+  const [detailedProduct, SetdetailedProduct] = useState([]);
   const { id } = useParams();
   const GetIdProduct = () => {
     axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
       console.log(res.data);
+      SetdetailedProduct(res.data);
     });
   };
   useEffect(GetIdProduct, []);
 
-  return <h1>prodotto {id}</h1>;
+  return (
+    <>
+      <h1>{detailedProduct.title}</h1>
+      <img src={detailedProduct.image} alt={detailedProduct.title} />
+      <p>{detailedProduct.description}</p>
+    </>
+  );
 }
